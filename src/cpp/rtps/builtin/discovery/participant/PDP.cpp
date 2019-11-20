86c334998d147f385cb0fb564f43992b89102015
Ricardo González <correoricky@gmail.com>
2019-11-20 16:25:07 +0100

    Removing unnecessary lock().

--- src/cpp/rtps/builtin/discovery/participant/PDP.cpp
+++ src/cpp/rtps/builtin/discovery/participant/PDP.cpp
@@ -1009,7 +1009,6 @@ void PDP::check_remote_participant_liveliness(
         {
             guard.unlock();
             remove_remote_participant(remote_participant->m_guid, ParticipantDiscoveryInfo::DROPPED_PARTICIPANT);
-            guard.lock();
             return;
         }
 
